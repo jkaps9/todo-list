80330d1f4dc4f1e6c5eaf32e9f3ca4c659706634
Josh <15219491+jkaps9@users.noreply.github.com>
2025-05-27 22:51:34 -0400

    Add ToDo class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import "./styles.css";
+import { ToDo } from "./todo.js";
+
+const toDoItem = new ToDo("Sleep", "Go to sleep", new Date(2025, 4, 27), "High");
+console.log(toDoItem);
---
+++ src/todo.js
@@ -0,0 +1,9 @@
+export class ToDo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.isComplete = false;
+    }
+}
