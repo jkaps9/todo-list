953da470094814bb9fb4766c4b4945f6079810b9
Josh <15219491+jkaps9@users.noreply.github.com>
2025-05-28 22:05:29 -0400

    Add makeTodoCard function to displayController

--- src/displayController.js
+++ src/displayController.js
@@ -19,7 +19,16 @@ export const displayController = (function () {
             listItem.appendChild(button);
             projectList.appendChild(listItem);
         });
+    };
 
+    const makeTodoCard = (todo) => {
+        const toDoCard = document.createElement("div");
+        toDoCard.classList.add("todo-item");
+
+        const checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+        checkBox.setAttribute("id", todo.title);
+        checkBox.setAttribute("name", todo.title);
     };
 
     return { clearProjectList, updateProjectList };
