501bf92eb69144c2c78c5cd3ca78769e4ecf8406
Josh <15219491+jkaps9@users.noreply.github.com>
2025-05-28 21:48:48 -0400

    Add updateProjectList func to displayController

--- src/displayController.js
+++ src/displayController.js
@@ -1,3 +1,5 @@
+import { Project } from "./project";
+
 export const displayController = (function () {
     const projectList = document.querySelector("#project-list");
 
@@ -7,5 +9,26 @@ export const displayController = (function () {
         }
     };
 
-    return { clearProjectList };
+    const updateProjectList = (arr) => {
+        if (typeof arr === Array) {
+            arr.forEach(element => {
+                if (typeof element === Project) {
+                    const listItem = document.createElement("li");
+                    const button = document.createElement("button");
+                    button.classList.add("project-btn");
+                    button.textContent = element.name;
+                    listItem.appendChild(button);
+                    projectList.appendChild(listItem);
+                } else {
+                    console.log(`Element is a ${Object.getPrototypeOf(element)} instead of a Project`);
+                }
+
+
+            });
+        } else {
+            console.log("Invalid type passed to updateProjectList function");
+        }
+    };
+
+    return { clearProjectList, updateProjectList };
 })();
